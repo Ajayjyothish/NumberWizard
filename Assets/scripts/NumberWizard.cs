799148cb462659c7f7aa9c8a65482ca549918319
LAPTOP-HT0THONL\Ajay <ajayjyothish007@gmail.com>
2018-08-05 09:12:48 +0530

    halfway

---
+++ Assets/scripts/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] Text text;
+    int guess;
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        guess = (min + max) / 2;
+        text.text = guess.ToString();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+        
+
+    }
+
+    public void onPressLower()
+    {
+        max = guess;
+        getGuess();
+        text.text = guess.ToString();
+        
+    }
+
+    public void onPressHigher()
+    {
+        min = guess;
+        getGuess();
+        text.text = guess.ToString();
+    }
+
+    public int getGuess()
+    {
+        guess = (min + max) / 2;
+        return guess;
+    }
+}
